098fe8b2a7894e35da23b204338281b51a7c0248
WMABABB <wm.andrew.babb@gmail.com>
2018-10-09 22:27:11 -0400

    heroku bug

--- server.js
+++ server.js
@@ -28,9 +28,19 @@ if (process.env.NODE_ENV === "production") {
 
 // Socket.io
 
-// app.get('*', (req,res) =>{
-//   res.sendFile(path.join(__dirname+'/client/build/index.html'));
-// });
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/build/index.html"));
+  });
+}
+
+else {
+  app.use(express.static(path.join(__dirname, '/client/public')));
+  app.get("/*", function(req, res) {
+    res.sendFile(path.join(__dirname, "./client/public/index.html"));
+  });
+}
 
 // const Sockets= require('./serverSockets.js');
 
